2084d88217d8d646790812f1d4d15ea6a317a26b
David Gaither <gaitherdb@gmail.com>
2021-10-12 07:17:12 -0400

    fixed spelling error. files now cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,7 @@ const FILES_TO_CACHE = [
     `/icons/icon-192x192.png`,
     `/icons/icon-512x512.png`,
     `/manifest.webmanifest`,
-    '/assets/css/style.css',
+    '/styles.css',
     'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
     'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
     
